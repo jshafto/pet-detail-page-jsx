6460df0689f5147ade990447232c63d2cc916d44
jshafto <jpshafto@gmail.com>
2020-09-15 17:11:51 -0400

    created all basic page components

---
+++ src/OwnerLink.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const OwnerLink = props =>
+    <a href={props.href}>
+        {`${props.lastName}, ${props.firstName}`}
+    </a>
+;
+
+export default OwnerLink;
--- src/PetDetails.js
+++ src/PetDetails.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 import PetDetailList from './PetDetailList'
+import OwnersList from './OwnersList'
 
 const PetDetails = props =>
-    <PetDetailList pet={props.pet}/>
+    <>
+        <PetDetailList pet={props.pet}/>
+        <OwnersList owners={props.pet.Owners}/>
+    </>
 ;
+
+PetDetails.defaultProps = {
+    pet: {
+        PetType: {},
+    },
+};
 
 export default PetDetails
